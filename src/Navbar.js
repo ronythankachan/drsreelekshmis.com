038c38245452427936bf56f057a02d5367ff7d6a
root <root@COGLPT016.localdomain>
2021-01-13 12:24:17 +0530

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <div className="navbar">
+            this is a navbar
+        </div>
+    )
+}
+
+export default Navbar
