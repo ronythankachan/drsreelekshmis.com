b162bc7126a393011217d6625df2a78b06f15167
ronythankachan <rony.mail2me@gmail.com>
2021-01-22 18:19:30 +0530

    temperorly replaced hero video with image

--- src/Hero.js
+++ src/Hero.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import './Hero.css';
 import HeroVideo from './video/hero_background.mp4';
+import Ayurvedha from './images/ayurvedha.jpg';
 import {Link} from 'react-router-dom';
 
 const Hero = () => {
     return (
         <div className="hero">
             <div className="hero__video">
-                <video autoPlay loop muted>
+                <img src={Ayurvedha} alt="ayurvedha"/>
+                {/* <video autoPlay loop muted>
                     <source src={HeroVideo} type="video/mp4"/>
-                </video>
+                </video> */}
             </div>
             <div className="hero__title">
                 <h1>Authentic kerala ayurveda clinic</h1>
                 <h4>An ayurveda care center offers authentic treatments and stuff</h4>
                 <div className="hero__buttons">
-                    <button><Link to={'/shop'} className="link">Buy medicines online</Link></button>
-                    <button><Link to={'/book_appointment'} className="link">Schedule a home appointment</Link></button> 
+                    <button><Link to={'/shop'} className="link black">Buy medicines online</Link></button>
+                    <button><Link to={'/book_appointment'} className="link black">Schedule a home appointment</Link></button> 
                 </div>
             </div>
         </div>
