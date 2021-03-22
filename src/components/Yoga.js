23d80ef6fda9092047c2761163b96dfa44086c1c
Rony Thankachan <rony.mail2me@gmail.com>
2021-03-23 01:24:02 +0530

    content style change

--- src/components/Yoga.js
+++ src/components/Yoga.js
@@ -6,16 +6,12 @@ import {Button} from 'react-bootstrap'
 const Yoga = () => {
     return (
         <div className="yoga">
-            <div className="main__heading">
+            <div className="content__title">
                 <h1>Therapeutic Yoga</h1>
-            </div>
-            <div className="content">
-                <div className="content__image">
-                    <img src={YogaGirl} alt="swarna prashana" />
-                </div>
+                <img src={YogaGirl} alt="swarna prashana" />
                 <p>Yoga is a life style. Most recent research prove that Yoga is not just a health practice but also a treatment modality for systemic disorders. We teach Yoga to kids and for the elderly for its health benefits. We also provide therapeutic yoga sessions for specific disorders like diabetes, hypertension, obesity, stress, depression, insomnia, menstrual disorders and hormonal problems. It also helps improve other musculoskeletal and neurological disorders like neck and back pain caused due to different disease conditions. Yoga along with herbal medicines helps improve the condition of the patient suffering with different diseases in a much faster way.</p>
-                <Button className="book__button" href='/book_appointment' variant="success">Book Appointment</Button>
             </div>
+            <Button className="book__button" href='/book_appointment' variant="success">Book Appointment</Button>
         </div>
     )
 }
