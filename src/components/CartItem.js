d860b460f6d7b3323bb9b8a46d21719d25d2edf4
Rony Thankachan <rony.mail2me@gmail.com>
2021-03-29 20:21:12 +0530

    cart initial design

---
+++ src/components/CartItem.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react'
+import './CartItem.css'
+import {Button} from 'react-bootstrap'
+
+
+const CartItem = ({data}) => {
+    const [quantity, setQuantity] = useState(1);
+
+    const addQuantity = () =>{
+        setQuantity(quantity+1)
+    }
+
+    const subtractQuantity = () =>{
+        if(quantity>1){
+            setQuantity(quantity-1)
+        }
+    }
+
+    return (
+        <div className="cartitem">
+            <div className="product__image">
+                <img src={data.img} alt=""/>
+            </div>
+            <div className="product__details">
+                <h3>{data.name} <small>({data.category})</small></h3> 
+                <div className="quantity">
+                    <Button style={{paddingTop:"0px"}}onClick={subtractQuantity} variant="secondary">-</Button>
+                    <h3>{quantity}</h3>
+                    <Button onClick={addQuantity} variant="secondary">+</Button>
+                </div>
+                <div className="total">
+                    <h3>Amount : {quantity * data.price}</h3>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CartItem
--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,9 +1,39 @@
 import React from 'react'
 import './CartPage.css'
+import PlaceHolder from '../images/Shop/placeholder.png'
+import CartItem from '../components/CartItem'
+
+const testData = [
+    {
+        id:1,
+        name:'Abhayarishtam',
+        category:'Arishtam',
+        description:'Abhayarishtam description',
+        price:120,
+        quantiy:23,
+        img:PlaceHolder
+
+    },
+    {
+        id:2,
+        name:'Ashta Choornam',
+        category:'Choornam',
+        description:'Ashta Choornam description',
+        price:320,
+        quantiy:49,
+        img:PlaceHolder
+    }
+]
+
+const products = testData.map(item => {
+    return <CartItem data={item}/>
+})
+
+
 const CartPage = () => {
     return (
         <div className="cartpage">
-            Cart Page
+            {products}
         </div>
     )
 }
