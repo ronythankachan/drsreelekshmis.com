aeed63bcba64885691f084d5377f1396f23d8350
Rony Thankachan <rony.mail2me@gmail.com>
2021-05-12 15:28:09 +0530

    signup page toggle completed

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,19 +1,37 @@
 import React,{useState} from 'react'
 import './LoginPage.css'
 
+const signUpData = {
+    titlebtn:'Sign up',
+    content:'Sign up to order medicines, view your booking history and manage your orders',
+    msg:'Have an account?',
+    subbtn:'Login'
+}
+const loginData = {
+    titlebtn:'Login',
+    content:'Login with you account to manage orders, order medicines etc',
+    msg:'Dont have an account?',
+    subbtn:'Sign up'
+}
+
 const Loginpage = () => {
     const [signUp, setSignUp] = useState(true)
+    const [infoData, setInfoData] =useState(signUpData)
+    const toggle = () =>{
+        signUp? setInfoData(loginData):setInfoData(signUpData);
+        setSignUp(!signUp)
+    }
     return (
         <div className="loginpage">
             <div className="loginpage__container">
                 <div className="loginpage__info">
                     <div className="loginpage__info__content">
-                        <button onClick={()=>setSignUp(true)}>Sign up</button>
-                        <small>Sign up to order medicines, view your booking history and manage your orders</small>
+                        <h2>{infoData.titlebtn}</h2>
+                        <small>{infoData.content}</small>
                     </div>
                     <div className="loginpage__info___loginbtn">
-                        <small style={{opacity:0.7}}>Have an account?</small>
-                        <button onClick={()=>setSignUp(false)}>Login</button>
+                        <small style={{opacity:0.7}}>{infoData.msg}</small>
+                        <button onClick={toggle}>{infoData.subbtn}</button>
                     </div>
                 </div>
                 <div className="loginpage__form">
