e43feb43e767349360b972a623c62012a6ca3062
rony thankachan <ronythankachan@ronys-MacBook-Air.local>
2021-06-19 03:53:12 +0530

    minor css changes

--- src/pages/MyAccountPage.js
+++ src/pages/MyAccountPage.js
@@ -3,8 +3,10 @@ import './MyAccountPage.css'
 import SidePanelContainer from '../components/SidePanelContainer'
 import {Form} from 'react-bootstrap'
 import { useState } from 'react'
-import { BiReset } from 'react-icons/bi';
+import { FiSettings } from 'react-icons/fi';
 import { FaShoppingBasket } from 'react-icons/fa';
+import { Button } from 'react-bootstrap'
+import { useFormik } from 'formik'
 
 
 const MyAccountPage = () => {
@@ -14,7 +16,7 @@ const MyAccountPage = () => {
             <SidePanelContainer>
                 <div className="sidepanel__items">
                     <button onClick={()=>setSidePanelControl('orders')}><FaShoppingBasket/>My Orders</button>
-                    <button onClick={()=>setSidePanelControl('resetPassword')}><BiReset/>Reset Password</button>
+                    <button onClick={()=>setSidePanelControl('resetPassword')}><FiSettings/>Settings</button>
                 </div>
             </SidePanelContainer>
             <PageContent sidePanelControl={sidePanelControl}/>
@@ -34,26 +36,40 @@ const PageContent = ({sidePanelControl}) =>{
 }
 
 const ResetPasswordForm = () =>{
+    const validate = values =>{
+        const errors = {}
+        if(!values.currentPassword)errors.currentPassword = "Enter current password"
+        if(!values.newPassword1) errors.newPassword1 = "Enter a new password"
+        if(!values.newPassword2) errors.newPassword2 = "Repeat the password"
+        if(values.newPassword1 !== values.newPassword2) errors.newPassword2 = "Passwords doesnt match"
+    }
+    const formik = useFormik({
+        initialValues:{
+            currentPassword:'',
+            newPassword1:'',
+            newPassword2:''
+        },
+        validate,
+        onSubmit: values =>{
+            console.log("submitting", values)
+        }
+    })
     return (
-        <div className="resetpassword">
-            <h3>Reset your password</h3>
-            <div className="resetpassword__form">
-                <Form >
-                    <Form.Group style={{display:"flex"}}>
-                        <Form.Label>Current Password</Form.Label>
-                        <Form.Control type="password" placeholder="Current Password"/>
+            <div className="resetpassword">
+                <h5 className="subheading">Reset Password</h5>
+                <div/>
+                <Form className="resetpassword__form" onSubmit={formik.handleSubmit}>
+                    <Form.Group>
+                        <Form.Label>Current Password *</Form.Label>
+                        <Form.Control name="currentPassword" value={formik.values.currentPassword} type="password" onChange={formik.handleChange}/>
+                        <Form.Label>New Password *</Form.Label>
+                        <Form.Control name="newPassword1" value={formik.values.newPassword1} type="password" onChange={formik.handleChange}/>
+                        <Form.Label>Re-enter New Password *</Form.Label>
+                        <Form.Control name="newPassword2" value={formik.values.newPassword2} type="password" onChange={formik.handleChange}/>
                     </Form.Group>
-                    <Form.Group style={{display:"flex"}}>
-                        <Form.Label>New Password</Form.Label>
-                        <Form.Control type="password" placeholder="New Password"/>
-                    </Form.Group>
-                    <Form.Group style={{display:"flex"}}>
-                        <Form.Label>Re-type Password</Form.Label>
-                        <Form.Control type="password" placeholder="Re-type Password"/>
-                    </Form.Group>
+                    <Button variant="primary" type="submit">Reset Password</Button>
                 </Form>
             </div>
-        </div>
     )
 }
 
