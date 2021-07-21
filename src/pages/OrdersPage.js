ce3892b13f9a003e5115ec722552f69f1cbaa74b
rony thankachan <ronythankachan@ronys-MacBook-Air.local>
2021-07-21 12:32:05 +0530

    myorders page wiring completed

--- src/pages/OrdersPage.js
+++ src/pages/OrdersPage.js
@@ -1,49 +1,67 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 import { ProgressBar } from "react-bootstrap";
 import LoadingButton from "../components/LoadingButton";
+import backend from "../axios";
 import "./OrdersPage.css";
 
-const orders = [
-  {
-    id: "18923434896",
-    date: "21 June 2021",
-    name: "Ashta Choornam",
-    price: 340,
-    quantity: 3,
-    status: "Processing",
-    img: "https://images.theconversation.com/files/256057/original/file-20190129-108364-17hlc1x.jpg",
-  },
-  {
-    id: "28923434895",
-    date: "21 June 2021",
-    name: "COVIShield Vaccine",
-    price: 240,
-    quantity: 2,
-    status: "Shipped",
-    img: "https://images.financialexpress.com/2021/02/covishield-2.jpg",
-  },
-  {
-    id: "1892734894",
-    date: "21 June 2021",
-    name: "Kanti Choornam",
-    price: 340,
-    quantity: 3,
-    status: "Delivered",
-    img: "https://img.etimg.com/thumb/msid-76075220,width-1200,height-900/industry/healthcare/biotech/pharmaceuticals/government-frees-exports-of-paracetamol-apis.jpg",
-  },
-];
+// const dummy = [
+//   {
+//     id: "18923434896",
+//     date: "21 June 2021",
+//     name: "Ashta Choornam",
+//     price: 340,
+//     quantity: 3,
+//     status: "Processing",
+//     img: "https://images.theconversation.com/files/256057/original/file-20190129-108364-17hlc1x.jpg",
+//   },
+//   {
+//     id: "28923434895",
+//     date: "21 June 2021",
+//     name: "COVIShield Vaccine",
+//     price: 240,
+//     quantity: 2,
+//     status: "Shipped",
+//     img: "https://images.financialexpress.com/2021/02/covishield-2.jpg",
+//   },
+//   {
+//     id: "1892734894",
+//     date: "21 June 2021",
+//     name: "Kanti Choornam",
+//     price: 340,
+//     quantity: 3,
+//     status: "Delivered",
+//     img: "https://img.etimg.com/thumb/msid-76075220,width-1200,height-900/industry/healthcare/biotech/pharmaceuticals/government-frees-exports-of-paracetamol-apis.jpg",
+//   },
+// ];
 const status = {
   Processing: 10,
   Shipped: 60,
   Delivered: 100,
 };
 const OrdersPage = () => {
+  // fetch orders of a particular user.
+  const [orders, setOrders] = useState();
+  useEffect(() => {
+    const fetchOrders = async () => {
+      const orders = await backend.get("api/get_orders", {
+        params: {
+          userId: localStorage.getItem("userId"),
+        },
+      });
+      setOrders(orders.data);
+    };
+    fetchOrders();
+  }, []);
+
   return (
     <div className="orders">
       <h4 className="subheading">Your Orders</h4>
-      {orders.map((order) => (
-        <Order order={order} />
-      ))}
+      {orders ? (
+        orders.map((order) => <Order order={order} />)
+      ) : (
+        <p>You haven't ordered anything yet.</p>
+      )}
     </div>
   );
 };
@@ -69,16 +87,16 @@ const Order = ({ order }) => {
       <div className="order__header">
         <div className="order__header__date">
           <p>ORDER PLACED</p>
-          <p>{order.date}</p>
+          <p>{new Date(order.orderDate).toDateString()}</p>
         </div>
         <div className="order__header__total">
           <p>TOTAL</p>
           <p>Rs. {order.price * order.quantity}</p>
         </div>
-        <div className="order__header__id">
+        {/* <div className="order__header__id">
           <p>ORDER ID</p>
-          <p>#{order.id}</p>
-        </div>
+          <p>#{order._id}</p>
+        </div> */}
       </div>
       <div className="order__body">
         <img src={order.img} alt="" />
@@ -88,15 +106,6 @@ const Order = ({ order }) => {
             <p>{order.quantity} items</p>
           </div>
           <div className="order__body__buttons">
-            {/* {order.status === "Processing" ? (
-              <Button size="sm" variant="dark" onClick={cancelOrder}>
-                Cancel Order
-              </Button>
-            ) : order.status === "Delivered" ? (
-              <Button size="sm" variant="dark" onClick={returnItem}>
-                Return Item
-              </Button>
-            ) : null} */}
             {order.status === "Processing" ? (
               <LoadingButton
                 onClick={cancelOrder}
