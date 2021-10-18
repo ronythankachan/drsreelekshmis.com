081e0acdae25888781e7c45a5eac069a5c5297eb
rony thankachan <ronythankachan@ronys-MacBook-Air.local>
2021-10-18 23:14:31 +0530

    address change and map updated

--- src/components/ClinicInfo.js
+++ src/components/ClinicInfo.js
@@ -1,24 +1,34 @@
-import React from 'react'
-import './ClinicInfo.css'
-import {Badge} from 'react-bootstrap'
+import React from "react";
+import "./ClinicInfo.css";
+import { Badge } from "react-bootstrap";
 
 const ClinicInfo = () => {
-    return (
-        <div className="clinicinfo">
-            <div className="info__item">
-                <Badge variant="primary" className="badge">Address</Badge> 
-                <p> First floor, Maruthi complex, SFS 407, 4th phase, Yelahanka New town, Bangalore 560064</p>
-            </div>
-            <div className="info__item">
-                <Badge variant="danger" className="badge">Phone</Badge> 
-                <p> +91 97404 76241</p>
-            </div>
-            <div className="info__item">
-                <Badge variant="dark" className="badge">Email</Badge> 
-                <p> drsreelekshmisclinic@gmail.com</p>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="clinicinfo">
+      <div className="info__item">
+        <Badge variant="primary" className="badge">
+          Address
+        </Badge>
+        <p>
+          {" "}
+          House No 244 SFS 407, 1st Main Rd, 4th Phase, Yelahanka New Town,
+          Bengaluru, Karnataka - 560064
+        </p>
+      </div>
+      <div className="info__item">
+        <Badge variant="danger" className="badge">
+          Phone
+        </Badge>
+        <p> +91 97404 76241</p>
+      </div>
+      <div className="info__item">
+        <Badge variant="dark" className="badge">
+          Email
+        </Badge>
+        <p> drsreelekshmisclinic@gmail.com</p>
+      </div>
+    </div>
+  );
+};
 
-export default ClinicInfo
+export default ClinicInfo;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,58 +1,112 @@
-import React from 'react';
-import './Footer.css';
-import {Link} from 'react-router-dom';
+import React from "react";
+import "./Footer.css";
+import { Link } from "react-router-dom";
 import { FaFacebook, FaInstagram } from "react-icons/fa";
-import {RiLinkedinBoxLine } from "react-icons/ri";
-import {AiOutlineGithub, AiOutlineCopyright } from "react-icons/ai";
-
+import { RiLinkedinBoxLine } from "react-icons/ri";
+import { AiOutlineGithub, AiOutlineCopyright } from "react-icons/ai";
 
 const Footer = () => {
-    return (
-        <div className="footer__container">
-            <div className="footer">
-                <div className="footer__left">
-                    <div className="footer__logo">
-                        <h2>
-                            <Link to={'/'} className="link logo_green">Dr.Sreelekshmi's clinic</Link>
-                        </h2>
-                    </div>
-                    <Link to={'/about'} className="link">About</Link>
-                    <Link to={'/contact'} className="link">Contact us</Link>
-                </div>
-                <div className="footer__middle">
-                    <div className="footer__heading">
-                        <h2>SERVICES</h2>
-                    </div>
-                    <Link to={'/rejuvenation'} className="link">Rejuvenation &amp; Pain Relief Therapies</Link>
-                    <Link to={'/post_delivery_care'} className="link">Pregnancy &amp; Post Delivery Care</Link>
-                    <Link to={'/panchakarma'} className="link">Panchakarma</Link>
-                    <Link to={'/swarna_prashana'} className="link">Swarna Prashana</Link>
-                    <Link to={'/yoga'} className="link">Yoga</Link>
-                </div>
-                <div className="footer__right">
-                    <div className="footer__heading">
-                        <h2>CONNECT WITH US</h2>
-                    </div>
-                    <p>Phone &amp; Whatsapp: <span style={{color:"lightgreen",fontWeight:"bold"}}>+91 97404 76241</span> </p>
-                    <p>Email: <span style={{color:"lightgreen"}}>drsreelekshmisclinic@gmail.com</span> </p>
-
-                    <p>First floor, Maruthi complex, SFS 407, 4th phase, Yelahanka New town, Bangalore 560064</p>
-                    <div className="footer__right__socialmedia">
-                        <a href="https://www.instagram.com/dr_srees_ayur_/" target="_blank" rel="noopener noreferrer"><FaFacebook/></a>
-                        <a href="https://www.facebook.com/dr.sreesAyur/" target="_blank" rel="noopener noreferrer"><FaInstagram/></a>
-                    </div>
-                </div>
-            </div>
-            <div className="shamelessplug">
-                    <p><AiOutlineCopyright/> This site is developed and managed by Rony Thankachan, 2021</p>
-                    <div className="shamelessplug__icons">
-                        <a href="https://in.linkedin.com/in/ronythankachan" target="_blank" rel="noopener noreferrer"><RiLinkedinBoxLine/></a>
-                        <a href="https://github.com/ronythankachan" target="_blank" rel="noopener noreferrer"><AiOutlineGithub/></a> 
-                    </div>
-            </div>
+  return (
+    <div className="footer__container">
+      <div className="footer">
+        <div className="footer__left">
+          <div className="footer__logo">
+            <h2>
+              <Link to={"/"} className="link logo_green">
+                Dr.Sreelekshmi's clinic
+              </Link>
+            </h2>
+          </div>
+          <Link to={"/about"} className="link">
+            About
+          </Link>
+          <Link to={"/contact"} className="link">
+            Contact us
+          </Link>
         </div>
+        <div className="footer__middle">
+          <div className="footer__heading">
+            <h2>SERVICES</h2>
+          </div>
+          <Link to={"/rejuvenation"} className="link">
+            Rejuvenation &amp; Pain Relief Therapies
+          </Link>
+          <Link to={"/post_delivery_care"} className="link">
+            Pregnancy &amp; Post Delivery Care
+          </Link>
+          <Link to={"/panchakarma"} className="link">
+            Panchakarma
+          </Link>
+          <Link to={"/swarna_prashana"} className="link">
+            Swarna Prashana
+          </Link>
+          <Link to={"/yoga"} className="link">
+            Yoga
+          </Link>
+        </div>
+        <div className="footer__right">
+          <div className="footer__heading">
+            <h2>CONNECT WITH US</h2>
+          </div>
+          <p>
+            Phone &amp; Whatsapp:{" "}
+            <span style={{ color: "lightgreen", fontWeight: "bold" }}>
+              +91 97404 76241
+            </span>{" "}
+          </p>
+          <p>
+            Email:{" "}
+            <span style={{ color: "lightgreen" }}>
+              drsreelekshmisclinic@gmail.com
+            </span>{" "}
+          </p>
 
-    )
-}
+          <p>
+            House No 244 SFS 407, 1st Main Rd, 4th Phase, <br />
+            Yelahanka New Town, Bengaluru, Karnataka-560064
+          </p>
+          <div className="footer__right__socialmedia">
+            <a
+              href="https://www.instagram.com/dr_srees_ayur_/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaFacebook />
+            </a>
+            <a
+              href="https://www.facebook.com/dr.sreesAyur/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="shamelessplug">
+        <p>
+          <AiOutlineCopyright /> This site is developed and managed by Rony
+          Thankachan, 2021
+        </p>
+        <div className="shamelessplug__icons">
+          <a
+            href="https://in.linkedin.com/in/ronythankachan"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <RiLinkedinBoxLine />
+          </a>
+          <a
+            href="https://github.com/ronythankachan"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <AiOutlineGithub />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
